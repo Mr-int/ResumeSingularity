8eb1d089f663af4ad7b514e0fd9ddd4ffea14f0e
Your Name <your.email@example.com>
2026-03-13 02:44:29 +0300

    hero бллок

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,11 @@
 import './hero.css';
 import searchIcon from '../../assets/icons/searchIcon.svg';
 import arrowIcon from "../../assets/icons/arrow.svg";
+import numbersBg from '../../assets/heroAnimation/numbers.png';
+import emojiBg from '../../assets/heroAnimation/emoji.png';
 import romanActive from '../../assets/heroAnimation/roman_active.png';
+import romanUnactive from '../../assets/heroAnimation/roman_unactive.png';
+import leraActive from '../../assets/heroAnimation/lera_active.png';
 import leraUnactive from '../../assets/heroAnimation/lera_unactive.png';
 import { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
@@ -41,17 +45,25 @@ const Hero = () => {
                     </Link>
                 </div>
 
-                {!isMobile && (
-                    <div className="hero__right">
+                <div className="hero__right">
+                        <div className="hero__right-bg" aria-hidden>
+                            <div className="hero__right-bg-item hero__right-bg-item--numbers">
+                                <img src={numbersBg} alt="" className="hero__right-bg-img" loading="lazy" />
+                            </div>
+                            <div className="hero__right-bg-item hero__right-bg-item--emoji">
+                                <img src={emojiBg} alt="" className="hero__right-bg-img" loading="lazy" />
+                            </div>
+                        </div>
                         <div className="hero__right-glow" aria-hidden />
                         <div className="hero__right-block hero__right-block--1">
-                            <img src={romanActive} alt="" loading="lazy" className="hero__right-block-img" />
+                            <img src={romanActive} alt="" loading="lazy" className="hero__right-block-img hero__right-block-img--default" />
+                            <img src={romanUnactive} alt="" loading="lazy" className="hero__right-block-img hero__right-block-img--hover" />
                         </div>
                         <div className="hero__right-block hero__right-block--2">
-                            <img src={leraUnactive} alt="" loading="lazy" className="hero__right-block-img" />
+                            <img src={leraUnactive} alt="" loading="lazy" className="hero__right-block-img hero__right-block-img--default" />
+                            <img src={leraActive} alt="" loading="lazy" className="hero__right-block-img hero__right-block-img--hover" />
                         </div>
                     </div>
-                )}
 
                 <img src={arrowIcon} alt="arrow" className="hero__arrow"/>
             </div>
