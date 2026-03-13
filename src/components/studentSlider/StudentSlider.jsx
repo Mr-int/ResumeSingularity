5a02a4f73d0e3d958858028f535920e433f3a8bc
Your Name <your.email@example.com>
2026-03-13 04:31:41 +0300

    StudentSlider.jsx фикс

--- src/components/studentSlider/StudentSlider.jsx
+++ src/components/studentSlider/StudentSlider.jsx
@@ -82,7 +82,7 @@ const StudentSlider = () => {
         }
     }
 
-    const ANIMATION_DURATION = 600;
+    const ANIMATION_DURATION = 700;
 
     const runSlideAnimation = (dir) => {
         if (animationTimeoutRef.current) clearTimeout(animationTimeoutRef.current);
